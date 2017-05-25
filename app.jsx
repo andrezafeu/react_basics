fa22834500e5a06a9f59458983862b7d62306bc7
Andreza Feu <andrezafeu@gmail.com>
2017-05-24 21:58:54 -0400

    Add component to return virtual DOM elements

--- app.jsx
+++ app.jsx
@@ -1,1 +1,11 @@
-ReactDOM.render(<h1>Hello!</h1>, document.getElementById('container'));
+function Application() {
+   {/* Components must return a single virtual DOM element, therefore the h1 and p are wrapped in a div */}
+  return (
+    <div>
+      <h1>Hello from React</h1>
+      <p>I was rendered from the Application component.</p>
+    </div>
+    );
+}
+
+ReactDOM.render(< Application />, document.getElementById('container'));
