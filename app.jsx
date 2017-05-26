ef1478c8e1156cb737bc87c65b95251b3fa1bf99
Andreza Feu <andrezafeu@gmail.com>
2017-05-25 21:18:46 -0400

    Mock up application

--- app.jsx
+++ app.jsx
@@ -1,9 +1,25 @@
 function Application() {
    {/* Components must return a single virtual DOM element, therefore the h1 and p are wrapped in a div */}
   return (
-    <div>
-      <h1>Hello from React</h1>
-      <p>I was rendered from the Application component.</p>
+    <div className='scoreboard'>
+      <div className='header'>
+        <h1>Scoreboard</h1>
+      </div>
+
+      <div className='players'>
+        <div className='player'>
+          <div className='player-name'>
+            Jane Doe
+          </div>
+          <div className='player-score'>
+            <div className='counter'>
+              <button className='counter-action decrement'> - </button>
+              <div className='counter-score'> 29 </div>
+              <button className='counter-action increment'> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     );
 }
