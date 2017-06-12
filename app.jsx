83e6de8f3dbb72753bddaaf0ba0ffe0da118c000
Andreza Feu <andrezafeu@gmail.com>
2017-06-11 23:53:46 -0400

    Pass initial score as property

--- app.jsx
+++ app.jsx
@@ -24,11 +24,13 @@ Header.propTypes = {
 };
 
 var Counter = React.createClass({
-  propTypes: {},
+  propTypes: {
+    initialScore: React.PropTypes.number.isRequired,
+  },
 
   getInitialState: function() {
     return {
-      score: 0,
+      score: this.props.initialScore,
     }
   },
   
@@ -62,7 +64,7 @@ function Player(props) {
         {props.name}
       </div>
       <div className='player-score'>
-        < Counter />
+        < Counter initialScore={props.score} />
       </div>
     </div>
   );
